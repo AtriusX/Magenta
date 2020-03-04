874821cf130a56c69b85f5ed9a34a1a691fd31de
Atrius <atriusedm@gmail.com>
2020-03-04 16:11:12 -0500

    Add example bot code

--- src/main/java/io/atrius/Bot.kt
+++ src/main/java/io/atrius/Bot.kt
@@ -1,5 +1,25 @@
 package io.atrius
 
+import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import net.dv8tion.jda.api.hooks.ListenerAdapter
+
+typealias Token = String
+
 fun main(args: Array<String>) {
-    TODO()
+    val api = if (args.isNotEmpty())
+        login(args[0])
+    else
+        error("Argument mismatch: [Token]")
+
+    api.addEventListener(Example())
+}
+
+fun login(token: Token, settings: JDABuilder.() -> Unit = {}): JDA =
+    JDABuilder(token).apply(settings).build()
+
+
+class Example : ListenerAdapter() {
+    override fun onMessageReceived(event: MessageReceivedEvent) = println(event.message.contentDisplay)
 }

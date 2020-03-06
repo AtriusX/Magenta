e5e6d4ea9ed0053e0827b86ba32eca96e3a8f9c6
Atrius <atriusedm@gmail.com>
2020-03-05 19:39:20 -0500

    Move bot login code into special bot manager

--- src/main/java/io/atrius/Bot.kt
+++ src/main/java/io/atrius/Bot.kt
@@ -1,29 +1,30 @@
 package io.atrius
 
-import net.dv8tion.jda.api.JDA
-import net.dv8tion.jda.api.JDABuilder
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent
-import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.dv8tion.jda.api.entities.Activity
+import net.dv8tion.jda.api.entities.MessageChannel
+import net.dv8tion.jda.api.entities.User
 
 typealias Token =
         String
 
 fun main(args: Array<String>) {
-    val api = if (args.isNotEmpty())
-        login(args[0])
+    if (args.isNotEmpty())
+        Bot.login(args[0])
     else
         error("Argument mismatch: [Token]")
-
-    api.addEventListener(example)
+    // Register commands
+    CommandManager.register(Example, Status)
 }
 
-fun login(token: Token, settings: JDABuilder.() -> Unit = {}): JDA =
-        JDABuilder(token).apply(settings).build().apply { addEventListener(CommandManager) }
+object Example : Command("ping") {
+    override fun execute(args: Array<String>, channel: MessageChannel, user: User) {
+        if (!user.isBot) channel.sendMessage("Pong!").submit(true)
+    }
+}
 
-
-val example = object : ListenerAdapter() {
-    override fun onMessageReceived(event: MessageReceivedEvent) {
-        if (!event.message.author.isBot)
-            event.channel.sendMessage(event.message.contentDisplay).submit(true)
+object Status : Command("status") {
+    override fun execute(args: Array<String>, channel: MessageChannel, user: User) {
+        if (args.isNotEmpty())
+            Bot.api.presence.setPresence(Activity.playing(args.joinToString(" ")), false)
     }
 }
--- src/main/java/io/atrius/Managers.kt
+++ src/main/java/io/atrius/Managers.kt
@@ -1,12 +1,25 @@
 package io.atrius
 
-import io.atrius.CommandType.PRIVATE
+import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.User
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import kotlin.math.min
 
-@Suppress("unused")
+object Bot {
+
+    lateinit var api: JDA
+        private set
+
+    fun login(token: Token, settings: JDABuilder.() -> Unit = {}) {
+        JDABuilder(token).apply(settings).build().apply {
+            addEventListener(CommandManager)
+            api = this
+        }
+    }
+}
+
 object CommandManager : Iterable<Command>, ListenerAdapter() {
 
     private val commands = arrayListOf<Command>()
@@ -31,14 +44,9 @@ object CommandManager : Iterable<Command>, ListenerAdapter() {
         // Loop over all commands
         for (c in this) {
             val (command, _, type) = c
-            // Check to see if the commands match
-            if (!content.startsWith("!$command", true)) {
+            // Skip if the commands or channel type don't match
+            if (!content.startsWith("!$command", true) || !type.meets(event.channelType))
                 continue
-            }
-            // Skip over commands that don't fit the right channel type
-            if (!event.channelType.isGuild && type == PRIVATE) {
-                continue
-            }
             // Execute the command
             c.execute(args.toTypedArray(), event.channel, event.author)
         }

0811fbabedf76eefd3f0ec9026ad28734447efd9
Atrius <atriusedm@gmail.com>
2020-03-10 03:43:43 -0400

    Fix for uptime command returning negative values

--- src/main/java/io/atrius/action/command/Uptime.kt
+++ src/main/java/io/atrius/action/command/Uptime.kt
@@ -4,13 +4,13 @@ import io.atrius.action.Command
 import net.dv8tion.jda.api.entities.MessageChannel
 import net.dv8tion.jda.api.entities.User
 import java.time.Duration
-import java.time.LocalTime
+import java.time.LocalDateTime
 
 object Uptime : Command("uptime") {
-    private val init = LocalTime.now()
+    private val init = LocalDateTime.now()
 
     override fun execute(args: Array<String>, channel: MessageChannel, user: User) {
-        val difference = Duration.between(init, LocalTime.now())
+        val difference = Duration.between(init, LocalDateTime.now())
         channel.sendMessage("Bot has been online for ${difference.toHours()} hours and ${difference.toMinutes() % 60} minutes.").submit()
     }
 }
